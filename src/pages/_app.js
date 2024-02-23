c1ea12d37b8ec099aee2a19294474052f317e253
ellosykageni <schlomokhalidi8@gmail.com>
2024-02-23 03:46:56 +0000

    first commit

---
+++ src/pages/_app.js
@@ -0,0 +1,15 @@
+import "@/styles/globals.css";
+import { Toaster } from "react-hot-toast";
+import { Layout } from "@/components";
+import { StateContext } from "../../Context/StateContext";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+  <Component {...pageProps} />
+  </Layout>
+    </StateContext>
+  )
+}
