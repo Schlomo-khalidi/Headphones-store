fcde39e294485a3fd42572f526747089402cea67
ellosykageni <schlomokhalidi8@gmail.com>
2024-03-04 04:16:12 +0000

    first-commit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>JS Mastery Store</title>
+        <title>Allan's Headphones Store</title>
       </Head>
       <header>
         <Navbar />
